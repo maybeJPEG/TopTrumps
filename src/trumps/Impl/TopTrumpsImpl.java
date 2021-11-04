a2da47d557828bf2f43e579a51506172f20dd58d
Lola Schmid <Lola.Schmid@Student.HTW-Berlin.de>
2021-11-04 15:47:48 +0100

    define methods of TopTrump interface

--- src/trumps/Impl/TopTrumpsImpl.java
+++ src/trumps/Impl/TopTrumpsImpl.java
@@ -6,9 +6,9 @@ import trumps.TopTrumps;
 import trumps.TopTrumpsClients;
 
 public class TopTrumpsImpl implements TopTrumps {
+
     @Override
-    public TopTrumpsClients pick(String userName, TopTrumpsClients wantedDeck)
-            throws GameExceptions, StatusException {
+    public TopTrumpsClients pick(String userName, TopTrumpsClients wantedDeck) throws GameExceptions, StatusException {
         return null;
     }
 
@@ -18,7 +18,13 @@ public class TopTrumpsImpl implements TopTrumps {
     }
 
     @Override
-    public CompareCategoryImpl compareCategory(int value, boolean isBest) throws GameExceptions {
+    public CompareCategoryImpl compareCategory(int CategoryAlice, int CategoryBob) throws GameExceptions {
+        return null;
+    }
+
+    @Override
+    public DistributeWonDeckImpl distributeWonDeck() throws GameExceptions {
         return null;
     }
 }
+
